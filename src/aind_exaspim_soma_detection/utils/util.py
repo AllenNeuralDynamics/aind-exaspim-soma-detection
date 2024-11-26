fbceaa77d320d8015fc8e304f29d455557179d60
anna-grim <anna.grim@alleninstitute.org>
2024-11-26 22:05:26 +0000

    major refactor: core proposal generation routines

---
+++ src/aind_exaspim_soma_detection/utils/util.py
@@ -0,0 +1,125 @@
+"""
+Created on Mon Nov 25 14:00:00 2024
+
+@author: Anna Grim
+@email: anna.grim@alleninstitute.org
+
+add description 
+
+"""
+
+from concurrent.futures import ThreadPoolExecutor
+
+import boto3
+import os
+import shutil
+
+
+def mkdir(path, delete=False):
+    """
+    Creates a directory at "path".
+
+    Parameters
+    ----------
+    path : str
+        Path of directory to be created.
+    delete : bool, optional
+        Indication of whether to delete directory at path if it already
+        exists. The default is False.
+
+    Returns
+    -------
+    None
+
+    """
+    if delete:
+        rmdir(path)
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+
+def rmdir(path):
+    """
+    Removes directory and all subdirectories at "path".
+
+    Parameters
+    ----------
+    path : str
+        Path to directory and subdirectories to be deleted if they exist.
+
+    Returns
+    -------
+    None
+
+    """
+    if os.path.exists(path):
+        shutil.rmtree(path)
+
+
+def write_to_s3(local_path, bucket_name, prefix):
+    """
+    Writes a single file on local machine to an s3 bucket.
+
+    Parameters
+    ----------
+    local_path : str
+        Path to file to be written to s3.
+    bucket_name : str
+        Name of s3 bucket.
+    prefix : str
+        Path within s3 bucket.
+
+    Returns
+    -------
+    None
+
+    """
+    s3 = boto3.client('s3')
+    s3.upload_file(local_path, bucket_name, prefix)
+
+
+# --- swc utils ---
+def write_points(output_dir, points, color=None, prefix=""):
+    util.mkdir(output_dir, delete=True)
+    with ThreadPoolExecutor() as executor:
+        # Assign Threads
+        threads = list()
+        for i, xyz in enumerate(points):
+            filename = prefix + str(i + 1) + ".swc"
+            path = os.path.join(output_dir, filename)
+            threads.append(
+                executor.submit(save_point, path, xyz, 10, color=color)
+            )
+
+
+def save_point(path, xyz, radius=5, color=None):
+    """
+    Writes an swc file.
+
+    Parameters
+    ----------
+    path : str
+        Path on local machine that swc file will be written to.
+    xyz : ArrayLike
+        xyz coordinate to be written to an swc file.
+    radius : float, optional
+        Radius of point. The default is 5um.
+    color : str, optional
+        Color of nodes. The default is None.
+
+    Returns
+    -------
+    None.
+
+    """
+    with open(path, "w") as f:
+        # Preamble
+        if color is not None:
+            f.write("# COLOR " + color)
+        else:
+            f.write("# id, type, x, y, z, r, pid")
+        f.write("\n")
+
+        # Entry
+        x, y, z = tuple(xyz)
+        f.write(f"1 5 {x} {y} {z} {radius} -1")
