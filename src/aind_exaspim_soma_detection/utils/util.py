80aaa57e379ac5705b000e00a51524151e89815a
Anna Grim <108307071+anna-grim@users.noreply.github.com>
2024-11-27 20:22:04 -0800

    feat: search for exaspim image path

--- src/aind_exaspim_soma_detection/utils/util.py
+++ src/aind_exaspim_soma_detection/utils/util.py
@@ -11,12 +11,14 @@ Miscellaneous helper routines.
 import os
 import shutil
 from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
 
 import boto3
 import numpy as np
 import pandas as pd
 
 
+# --- os utils ---
 def mkdir(path, delete=False):
     """
     Creates a directory at "path".
@@ -56,28 +58,6 @@ def rmdir(path):
     """
     if os.path.exists(path):
         shutil.rmtree(path)
-
-
-def write_to_s3(local_path, bucket_name, prefix):
-    """
-    Writes a single file on local machine to an s3 bucket.
-
-    Parameters
-    ----------
-    local_path : str
-        Path to file to be written to s3.
-    bucket_name : str
-        Name of s3 bucket.
-    prefix : str
-        Path within s3 bucket.
-
-    Returns
-    -------
-    None
-
-    """
-    s3 = boto3.client("s3")
-    s3.upload_file(local_path, bucket_name, prefix)
 
 
 # --- Extracting somas from smartsheets ---
@@ -221,3 +201,200 @@ def save_point(path, xyz, radius=5, color=None):
         # Entry
         x, y, z = tuple(xyz)
         f.write(f"1 5 {x} {y} {z} {radius} -1")
+
+
+# --- S3 utils ---
+def list_s3_prefixes(bucket_name, prefix):
+    """
+    Lists all immediate subdirectories of a given S3 path (prefix).
+
+    Parameters
+    -----------
+    bucket_name : str
+        Name of the S3 bucket to search.
+    prefix : str
+        S3 prefix (path) to search within.
+
+    Returns:
+    --------
+    List[str]
+        List of immediate subdirectories under the specified prefix.
+
+    """
+    # Check prefix is valid
+    if not prefix.endswith("/"):
+        prefix += "/"
+
+    # Call the list_objects_v2 API
+    s3 = boto3.client("s3")
+    response = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix, Delimiter="/")
+    subprefixes = []
+    if "CommonPrefixes" in response:
+        subprefixes = [cp["Prefix"] for cp in response["CommonPrefixes"]]
+    return subprefixes
+
+
+def list_s3_bucket_prefixes(bucket_name, keyword=None):
+    """
+    Lists all top-level prefixes (directories) in an S3 bucket, optionally
+    filtering by a keyword.
+
+    Parameters
+    -----------
+    bucket_name : str
+        Name of the S3 bucket to search.
+    keyword : str, optional
+        Keyword used to filter the prefixes.
+
+    Returns
+    --------
+    List[str]
+        A list of top-level prefixes (directories) in the S3 bucket. If a
+        keyword is provided, only the matching prefixes are returned.
+
+    """
+    # Initializations
+    prefixes = []
+    continuation_token = None
+    s3 = boto3.client("s3")
+
+    # Main
+    while True:
+        # Call the list_objects_v2 API
+        list_kwargs = {"Bucket": bucket_name, "Delimiter": "/"}
+        if continuation_token:
+            list_kwargs["ContinuationToken"] = continuation_token
+        response = s3.list_objects_v2(**list_kwargs)
+
+        # Collect the top-level prefixes
+        if "CommonPrefixes" in response:
+            for prefix in response["CommonPrefixes"]:
+                if keyword and keyword in prefix["Prefix"].lower():
+                    prefixes.append(prefix["Prefix"])
+                elif keyword is None:
+                    prefixes.append(prefix["Prefix"])
+
+        # Check if there are more pages to fetch
+        if response.get("IsTruncated"):
+            continuation_token = response.get("NextContinuationToken")
+        else:
+            break
+    return prefixes
+
+
+def is_file_in_prefix(bucket_name, prefix, filename):
+    """
+    Checks if a specific file exists within a given S3 prefix.
+
+    Parameters
+    -----------
+    bucket_name : str
+        Name of the S3 bucket to searched.
+    prefix : str
+        S3 prefix (path) under which to look for the file.
+    filename : str
+        Name of the file to search for within the specified prefix.
+
+    Returns:
+    --------
+    bool
+        Returns "True" if the file exists within the given prefix,
+        otherwise "False".
+
+    """
+    
+    for sub_prefix in list_s3_prefixes(bucket_name, prefix):
+        if filename in sub_prefix:
+            return True
+    return False
+
+
+def write_to_s3(local_path, bucket_name, prefix):
+    """
+    Writes a single file on local machine to an s3 bucket.
+
+    Parameters
+    ----------
+    local_path : str
+        Path to file to be written to s3.
+    bucket_name : str
+        Name of s3 bucket.
+    prefix : str
+        Path within s3 bucket.
+
+    Returns
+    -------
+    None
+
+    """
+    s3 = boto3.client("s3")
+    s3.upload_file(local_path, bucket_name, prefix)
+
+
+def find_img_prefix(bucket_name, prefixes, sample_id):
+    cur_prefix = None
+    cur_date = None
+    for prefix in prefixes:
+        # Check if prefix is valid
+        if str(sample_id) in prefix and "fusion" in prefix:
+            # Check if prefix has fused image
+            if is_file_in_prefix(bucket_name, prefix, "fused.zarr"):
+                # Check if prefix is most recent
+                date = get_upload_date(prefix)
+                if cur_prefix is None:
+                    cur_prefix = prefix
+                    cur_date = date
+                elif date == most_recent_date(date, cur_date):
+                    cur_prefix = prefix
+                    cur_date = date
+    return cur_prefix + "fused.zarr/" if cur_prefix is not None else cur_prefix
+
+
+# --- Miscellaneous ---
+def get_upload_date(prefix):
+    """
+    Extracts the upload date from an S3 prefix string.
+
+    Parameters
+    -----------
+    prefix : str
+        S3 prefix (path) containing the upload date in the format
+        "<name>/fusion_<date>/".
+
+    Returns
+    --------
+    str
+        Extracted date string without slashes.
+
+    """
+    date = prefix.split("fusion_")[-1]
+    return date.replace("/", "")
+
+
+def most_recent_date(date1_str, date2_str, date_format="%Y-%m-%d_%H-%M-%S"):
+    """
+    Compares two date strings and determines which one is more recent.
+
+    Parameters
+    -----------
+    date1_str : str
+        First date string to compare.
+    date2_str : str
+        Second date string to compare.
+    
+    date_format : str, optional
+        The format in which the dates are provided. The default is
+        "%Y-%m-%d %H:%M:%S".
+    
+    Returns
+    --------
+    str
+       More recent data in the same given format.
+
+    """
+    try:
+        date1 = datetime.strptime(date1_str, date_format)
+        date2 = datetime.strptime(date2_str, date_format)
+        return date1_str if date1 > date2 else date2_str
+    except ValueError:
+        return ""
