97e4c4bdeeb23b451f6b66b9cd9e8ad0a206e9c1
Anna Grim <108307071+anna-grim@users.noreply.github.com>
2024-12-06 18:01:27 -0800

    feat: load exaspim dataset

--- src/aind_exaspim_soma_detection/training/data_handling.py
+++ src/aind_exaspim_soma_detection/training/data_handling.py
@@ -9,6 +9,8 @@ Created on Mon Dec 5 14:00:00 2024
 
 from torch.utils.data import Dataset
 
+import os
+
 from aind_exaspim_soma_detection.utils import img_util, util
 
 
@@ -80,26 +82,90 @@ class SomaDataLoader:
 
 
 # --- Fetch Training Data ---
-def fetch_smartsheet_somas(
-    smartsheet_path,
-    img_lookup_path,
-    anisotropy=[1.0, 1.0, 1.0],
-    multiscale=0,
-):
+def fetch_smartsheet_somas(smartsheet_path, img_prefixes_path, multiscale=0):
     # Read data
     soma_coords = util.extract_somas_from_smartsheet(smartsheet_path)
-    img_name_lookup = util.read_json(img_lookup_path)
+    img_prefixes = util.read_json(img_prefixes_path)
 
     # Reformat data
     data = list()
     for brain_id, xyz_list in soma_coords.items():
-        img_name = img_name_lookup[brain_id]
-        img_path = f"s3://aind-open-data/{img_name}/fused.zarr/{multiscale}"
-        voxels = [img_util.to_voxels(xyz, multiscale) for xyz in xyz_list]
-        labels = len(soma_coords[brain_id]) * [1]
-        data.append((brain_id, img_path, voxels, labels))
+        data.append(
+            reformat_data(brain_id, img_prefixes, multiscale, xyz_list, 1)
+        )
     return data
 
 
-def fetch_soma_nonsoma_2024():
-    pass
+def fetch_exaspim_somas_2024(dataset_path, img_prefixes_path, multiscale=0):
+    """
+    Fetches and formats soma data for training from the exaSPIM dataset.
+
+    Parameters
+    ----------
+    dataset_path : str
+        Path to the dataset directory containing brain-specific subdirectories
+        with "accepts" and "rejects" folders.
+    img_prefixes_path : str
+        Path to a JSON file that maps brain IDs to image S3 prefixes.
+    multiscale : int, optional
+        Level in the image pyramid that the voxel coordinates must index into.
+
+    Returns
+    -------
+    List[tuple]
+        List of tuples where each tuple contains the following:
+            - "brain_id" (str): The unique identifier for the brain.
+            - "img_path" (str): Path to image stored in S3 image.
+            - "voxels" (list): Voxel coordinates of proposed somas.
+            - "labels" (list): Labels corresponding to voxels.
+
+    """
+    data = list()
+    img_prefixes = util.read_json(img_prefixes_path)
+    for brain_id in util.list_subdirectory_names(dataset_path):
+        # Read data
+        accepts_dir = os.path.join(dataset_path, brain_id, "accepts")
+        accepts_xyz = util.read_swc_dir(accepts_dir)
+
+        rejects_dir = os.path.join(dataset_path, brain_id, "rejects")
+        rejects_xyz = util.read_swc_dir(rejects_dir)
+
+        # Reformat data
+        data.append(
+            reformat_data(brain_id, img_prefixes, multiscale, accepts_xyz, 1)
+        )
+        data.append(
+            reformat_data(brain_id, img_prefixes, multiscale, rejects_xyz, 0)
+        )
+    return data
+
+
+def reformat_data(brain_id, img_prefixes, multiscale, xyz_list, label):
+    """
+    Reformats data for training or inference by converting xyz to voxel
+    coordinates and associates them with a brain id, image path, and labels.
+
+    Parameters
+    ----------
+    brain_id : str
+        Unique identifier for the whole brain dataset.
+    img_prefixes : dict
+        A dictionary mapping brain IDs to image S3 prefixes.
+    multiscale : int
+        Level in the image pyramid that the voxel coordinates must index into.
+    xyz_list : List[ArrayLike]
+        List 3D coordinates (x, y, z).
+    label : int
+        Label associated with the given coordinates (i.e. 1 for "accepts" and
+        0 for "rejects").
+
+    Returns
+    -------
+    tuple
+        Tuple containing the "brain_id", "image_path", "voxels", and "labels".
+
+    """
+    img_path = img_prefixes[brain_id] + str(multiscale)
+    voxels = [img_util.to_voxels(xyz, multiscale) for xyz in xyz_list]
+    labels = len(voxels) * [label]
+    return (brain_id, img_path, voxels, labels)
