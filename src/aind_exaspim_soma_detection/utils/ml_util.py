7216b1cd593f69772ed5bbc173a8e93e5d4f1a04
Anna Grim <108307071+anna-grim@users.noreply.github.com>
2025-02-06 14:25:17 -0800

    feat: false/true positive/negative extracter

--- src/aind_exaspim_soma_detection/utils/ml_util.py
+++ src/aind_exaspim_soma_detection/utils/ml_util.py
@@ -18,6 +18,117 @@ from sklearn.metrics import (
 )
 
 
+def get_correct(keys, y, hat_y, threshold, verbose=True):
+    """
+    Identifies true positives and true negatives from a prediction.
+
+    Parameters
+    ----------
+    keys : List[tuple]
+        Identifiers corresponding to each example.
+    y : List[int]
+        Ground truth labels for each example.
+    hat_y : list of float
+        Predicted likelihoods for each example.
+    threshold : float
+        Threshold value for classifying the predicted likelihoods.
+    verbose : bool, optional
+        Indication of whether to print our the number of true positives and
+        true negatives. The default is True.
+
+    Returns
+    -------
+    dict
+        A dictionary with the keys "true_negatives" and "true_positives" and
+        values that consist of tuples containing a key and predicted value.
+
+    """
+    # Extract incorrect
+    correct = {"true_negatives": list(), "true_positives": list()}
+    for i, (y_i, hat_y_i) in enumerate(zip(y, hat_y)):
+        if y_i == 0 and hat_y_i < threshold:
+            correct["true_negatives"].append((keys[i], hat_y_i))
+        elif y_i == 1 and hat_y_i > threshold:
+            correct["true_positives"].append((keys[i], hat_y_i))
+
+    # Report results
+    if verbose:
+        n_true_negatives = len(correct["true_negatives"])
+        n_true_positives = len(correct["true_positives"])
+        print(f"# True Positives: {n_true_positives}")
+        print(f"# True Negatives: {n_true_negatives}")
+    return correct
+
+
+def get_incorrect(keys, y, hat_y, threshold, verbose=True):
+    """
+    Identifies false positives and false negatives from a prediction.
+
+    Parameters
+    ----------
+    keys : List[tuple]
+        Identifiers corresponding to each example.
+    y : List[int]
+        Ground truth labels for each example.
+    hat_y : list of float
+        Predicted likelihoods for each example.
+    threshold : float
+        Threshold value for classifying the predicted likelihoods.
+    verbose : bool, optional
+        Indication of whether to print our the number of false positives and
+        false negatives. The default is True.
+
+    Returns
+    -------
+    dict
+        A dictionary with the keys "false_negatives" and "false_positives" and
+        values that consist of tuples containing a key and predicted value.
+
+    """
+    # Extract incorrect
+    incorrect = {"false_negatives": list(), "false_positives": list()}
+    for i, (y_i, hat_y_i) in enumerate(zip(y, hat_y)):
+        if y_i == 1 and hat_y_i < threshold:
+            incorrect["false_negatives"].append((keys[i], hat_y_i))
+        elif y_i == 0 and hat_y_i > threshold:
+            incorrect["false_positives"].append((keys[i], hat_y_i))
+
+    # Report results
+    if verbose:
+        n_false_negatives = len(incorrect["false_negatives"])
+        n_false_positives = len(incorrect["false_positives"])
+        print(f"# False Positives: {n_false_positives}")
+        print(f"# False Negatives: {n_false_negatives}")
+    return incorrect
+
+
+def report_metrics(y, hat_y, threshold):
+    """
+    Computes and prints various evaluation metrics based on the true labels
+    "y" and predicted labels "hat_y".
+
+    Parameters
+    ----------
+    y : numpy.ndarray
+        True labels.
+    hat_y : numpy.ndarray
+        Predicted labels from a machine learning model.
+
+    Returns
+    -------
+    None
+
+    """
+    hat_y = (hat_y > threshold).astype(int)
+    accuracy = accuracy_score(y, hat_y)
+    print("Accuracy:", round(accuracy, 4))
+    print("Accuracy Dif:", round(accuracy - np.sum(y) / len(y), 4))
+    print("Precision:", round(precision_score(y, hat_y), 4))
+    print("Recall:", round(recall_score(y, hat_y), 4))
+    print("F1:", round(f1_score(y, hat_y), 4))
+    print("")
+
+
 def split_train_validation(examples, train_ratio=0.85):
     """
     Splits a dictionary of examples into training and validation sets based on
@@ -49,33 +160,6 @@ def split_train_validation(examples, train_ratio=0.85):
     train_examples = dict({k: examples[k] for k in train_keys})
     valid_examples = dict({k: examples[k] for k in valid_keys})
     return train_examples, valid_examples
-
-
-def report_metrics(y, hat_y, threshold):
-    """
-    Computes and prints various evaluation metrics based on the true labels
-    "y" and predicted labels "hat_y".
-
-    Parameters
-    ----------
-    y : numpy.ndarray
-        True labels.
-    hat_y : numpy.ndarray
-        Predicted labels from a machine learning model.
-
-    Returns
-    -------
-    None
-
-    """
-    hat_y = (hat_y > threshold).astype(int)
-    accuracy = accuracy_score(y, hat_y)
-    print("Accuracy:", round(accuracy, 4))
-    print("Accuracy Dif:", round(accuracy - np.sum(y) / len(y), 4))
-    print("Precision:", round(precision_score(y, hat_y), 4))
-    print("Recall:", round(recall_score(y, hat_y), 4))
-    print("F1:", round(f1_score(y, hat_y), 4))
-    print("")
 
 
 def toCPU(tensor, return_numpy=True):
