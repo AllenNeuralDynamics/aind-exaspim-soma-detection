81cbfc06dce37b8b6e4a209f5426b8037e633c34
Anna Grim <108307071+anna-grim@users.noreply.github.com>
2025-06-06 13:46:29 -0700

    Update soma_proposal_classification.py

--- src/aind_exaspim_soma_detection/soma_proposal_classification.py
+++ src/aind_exaspim_soma_detection/soma_proposal_classification.py
@@ -195,7 +195,7 @@ def compute_metrics(
 
     """
     voxels = [img_util.to_voxels(p, multiscale) for p in accepts]
-    with ThreadPoolExecutor(max_workers=16) as executor:
+    with ThreadPoolExecutor(max_workers=2) as executor:
         # Assign threads
         threads = list()
         for voxel in voxels:
